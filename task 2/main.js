804e06c4a2a48fd7cc9c58e19f0315d16d0a0910
LegjobbCenter <kalmanbarnabas4@gamil.com>
2022-05-03 23:13:44 +0200

    main

--- task 2/main.js
+++ task 2/main.js
@@ -1,5 +1,5 @@
-const car ={brand:"Ford" , model:"f-150" ,type:"Raptor"}
-
-function getModel() {
-    console.log(car.model);
+const car = {brand:"Ford" , model:"f-150" ,type:"Raptor"}
+const car2 = {brand:"Mazda" , model: "mx5" , type: "nd"} 
+function getModel(obj) {
+    console.log(obj.model);
 }
